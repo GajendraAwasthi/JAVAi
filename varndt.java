70c8449aa86001d474ff73cf66b4ce55596a71ad
GajendraAwasthi <159510549+GajendraAwasthi@users.noreply.github.com>
2025-09-19 17:41:45 +0545

    learned about basic datatypes that are commonly used in  JAVA

---
+++ varndt.java
@@ -0,0 +1,33 @@
+/*the term varndt that i created is means variable and datatypes.
+Here How to assign value,
+syntax: 
+datatype variable_name = value ;
+*/
+package JAVAi; //  the package is just folder name
+
+public class varndt {
+    public static void main(String[] args) {
+        System.out.println("Hello We Are Using different datatypes Like integer, float , long , char , string 😁");
+
+        //entering int datatype
+        int var = 20;//the value of int shouldnt be in any quotes.
+        System.out.println(var);
+        
+        //using float datatypes
+        float pie = 3.25f; //the value of float shouldnt be in any quotes
+        System.out.println(pie);
+
+        //entering long datatypes
+        Long numb = 98989898L;//the value of Long shouldnt be in any quotes and at back if long integer there should be "L" eg: 123456L
+        System.out.println(numb);
+
+        //using char datatype
+        char vowel = 'A';//the value of char be in single quotes
+        System.out.println(vowel);
+
+        //entering string datatype
+        String text = "Hello Everyone I am God";//the value of float should be in double any quotes
+        System.out.println(text);
+
+    }
+}
