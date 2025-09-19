9f1abd03d04892933b1ea0d126675a413daba059
GajendraAwasthi <159510549+GajendraAwasthi@users.noreply.github.com>
2025-09-19 17:50:01 +0545

    learn code formatting
    here i learn how to wirte formatted output in java like in c i use printf("the value of number is %d"&num);

--- varndt.java
+++ varndt.java
@@ -11,23 +11,23 @@ public class varndt {
 
         //entering int datatype
         int var = 20;//the value of int shouldnt be in any quotes.
-        System.out.println(var);
+        System.out.println("The value of int is: "+var);
         
         //using float datatypes
         float pie = 3.25f; //the value of float shouldnt be in any quotes
-        System.out.println(pie);
+        System.out.println("The value of float is: "+pie);
 
         //entering long datatypes
         Long numb = 98989898L;//the value of Long shouldnt be in any quotes and at back if long integer there should be "L" eg: 123456L
-        System.out.println(numb);
+        System.out.println("The value of Long is: "+numb);
 
         //using char datatype
         char vowel = 'A';//the value of char be in single quotes
-        System.out.println(vowel);
+        System.out.println("The value of char is: "+vowel);
 
         //entering string datatype
-        String text = "Hello Everyone I am God";//the value of float should be in double any quotes
-        System.out.println(text);
+        String text = "Hello Everyone 😁";//the value of float should be in double any quotes
+        System.out.println("The value of String is: "+text);
 
     }
 }
